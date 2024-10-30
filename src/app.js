8eb9a2079336f3499206250be40553fa52fe12d2
Ruzbihan <ruzbihanmzm@gmail.com>
2024-10-30 21:37:39 +0530

    index and app files

---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const jokesRoutes = require('./routes/jokesRoutes');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/moderate/jokes', jokesRoutes);
+
+module.exports = app;
