6c1aa04092ca9518718a75389b4f62b208122420
Ruzbihan <ruzbihanmzm@gmail.com>
2024-10-31 19:23:20 +0530

    lint issues fixes

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -9,7 +9,7 @@ const auth = (req, res, next) => {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
-  } catch (err) {
+  } catch {
     res.status(401).json({ message: "Token is not valid" });
   }
 };
